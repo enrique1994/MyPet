9bb37ac088f11821733c547e4d6fb990395b93c3
Juan Manuel Gonzalez Calleros <jumagoca78@users.noreply.github.com>
2017-07-08 10:53:00 -0500

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyPet
+//
+//  Created by Milab on 08/07/17.
+//  Copyright © 2017 Enrique. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
